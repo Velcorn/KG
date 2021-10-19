981d282116a155a05c52a40b650b7d0c082e1f92
Velcorn <Velcorn@users.noreply.github.com>
2021-10-19 14:57:56 +0200

    Initial commit

---
+++ KG_01.py
@@ -0,0 +1,11 @@
+from rdflib import Graph
+
+
+def read_rdf():
+    g = Graph()
+    g.parse("simpsons.ttl")
+    return f"The model has {len(g)} triples."
+
+
+if __name__ == '__main__':
+    print(read_rdf())
